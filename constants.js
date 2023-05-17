eff6a8978337bbaf29805565f5c450f9a9e51702
Scott Kosman <kosman.scott@gmail.com>
2023-05-17 14:40:40 -0400

    update colors away from awful red/yellow/green mistake

--- constants.js
+++ constants.js
@@ -1,66 +1,86 @@
 // @flow
-import * as d3 from 'd3'
+import * as d3 from "d3";
 
 // Track copy moved to separate .json file
-import alltracks from './copy/tracks';
+import alltracks from "./copy/tracks";
 
-export type TrackId = 'MOBILE' | 'FRONTEND' | 'DEVOPS' | 'BACKEND' |
-  'PROJECT_MANAGEMENT' | 'COMMUNICATION' | 'CRAFT' | 'INITIATIVE' |
-  'CAREER_DEVELOPMENT' | 'ORG_DESIGN' | 'WELLBEING' | 'ACCOMPLISHMENT' |
-  'MENTORSHIP' | 'EVANGELISM' | 'RECRUITING' | 'COMMUNITY'
-export type Milestone = 0 | 1 | 2 | 3 | 4 | 5
+export type TrackId =
+  | "MOBILE"
+  | "FRONTEND"
+  | "DEVOPS"
+  | "BACKEND"
+  | "PROJECT_MANAGEMENT"
+  | "COMMUNICATION"
+  | "CRAFT"
+  | "INITIATIVE"
+  | "CAREER_DEVELOPMENT"
+  | "ORG_DESIGN"
+  | "WELLBEING"
+  | "ACCOMPLISHMENT"
+  | "MENTORSHIP"
+  | "EVANGELISM"
+  | "RECRUITING"
+  | "COMMUNITY";
+export type Milestone = 0 | 1 | 2 | 3 | 4 | 5;
 
 export type MilestoneMap = {
-  'MOBILE': Milestone,
-  'FRONTEND': Milestone,
-  'DEVOPS': Milestone,
-  'BACKEND': Milestone,
-  'PROJECT_MANAGEMENT': Milestone,
-  'COMMUNICATION': Milestone,
-  'CRAFT': Milestone,
-  'INITIATIVE': Milestone,
-  'CAREER_DEVELOPMENT': Milestone,
-  'ORG_DESIGN': Milestone,
-  'WELLBEING': Milestone,
-  'ACCOMPLISHMENT': Milestone,
-  'MENTORSHIP': Milestone,
-  'EVANGELISM': Milestone,
-  'RECRUITING': Milestone,
-  'COMMUNITY': Milestone
-}
-export const milestones = [0, 1, 2, 3, 4, 5]
+  MOBILE: Milestone,
+  FRONTEND: Milestone,
+  DEVOPS: Milestone,
+  BACKEND: Milestone,
+  PROJECT_MANAGEMENT: Milestone,
+  COMMUNICATION: Milestone,
+  CRAFT: Milestone,
+  INITIATIVE: Milestone,
+  CAREER_DEVELOPMENT: Milestone,
+  ORG_DESIGN: Milestone,
+  WELLBEING: Milestone,
+  ACCOMPLISHMENT: Milestone,
+  MENTORSHIP: Milestone,
+  EVANGELISM: Milestone,
+  RECRUITING: Milestone,
+  COMMUNITY: Milestone
+};
+export const milestones = [0, 1, 2, 3, 4, 5];
 
 export const milestoneToPoints = (milestone: Milestone): number => {
   switch (milestone) {
-    case 0: return 0
-    case 1: return 1
-    case 2: return 3
-    case 3: return 6
-    case 4: return 12
-    case 5: return 20
-    default: return 0
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    case 2:
+      return 3;
+    case 3:
+      return 6;
+    case 4:
+      return 12;
+    case 5:
+      return 20;
+    default:
+      return 0;
   }
-}
+};
 
 export const pointsToLevels = {
-  '0': '1.1',
-  '5': '1.2',
-  '11': '1.3',
-  '17': '2.1',
-  '23': '2.2',
-  '29': '2.3',
-  '36': '3.1',
-  '43': '3.2',
-  '50': '3.3',
-  '58': '4.1',
-  '66': '4.2',
-  '74': '4.3',
-  '90': '5.1',
-  '110': '5.2',
-  '135': '5.3',
-}
+  "0": "1.1",
+  "5": "1.2",
+  "11": "1.3",
+  "17": "2.1",
+  "23": "2.2",
+  "29": "2.3",
+  "36": "3.1",
+  "43": "3.2",
+  "50": "3.3",
+  "58": "4.1",
+  "66": "4.2",
+  "74": "4.3",
+  "90": "5.1",
+  "110": "5.2",
+  "135": "5.3"
+};
 
-export const maxLevel = 135
+export const maxLevel = 135;
 
 export type Track = {
   displayName: string,
@@ -71,57 +91,64 @@ export type Track = {
     signals: string[],
     examples: string[]
   }[]
-}
+};
 
 type Tracks = {|
-  'MOBILE': Track,
-  'FRONTEND': Track,
-  'DEVOPS': Track,
-  'BACKEND': Track,
-  'PROJECT_MANAGEMENT': Track,
-  'COMMUNICATION': Track,
-  'CRAFT': Track,
-  'INITIATIVE': Track,
-  'CAREER_DEVELOPMENT': Track,
-  'ORG_DESIGN': Track,
-  'WELLBEING': Track,
-  'ACCOMPLISHMENT': Track,
-  'MENTORSHIP': Track,
-  'EVANGELISM': Track,
-  'RECRUITING': Track,
-  'COMMUNITY': Track
-|}
+  MOBILE: Track,
+  FRONTEND: Track,
+  DEVOPS: Track,
+  BACKEND: Track,
+  PROJECT_MANAGEMENT: Track,
+  COMMUNICATION: Track,
+  CRAFT: Track,
+  INITIATIVE: Track,
+  CAREER_DEVELOPMENT: Track,
+  ORG_DESIGN: Track,
+  WELLBEING: Track,
+  ACCOMPLISHMENT: Track,
+  MENTORSHIP: Track,
+  EVANGELISM: Track,
+  RECRUITING: Track,
+  COMMUNITY: Track
+|};
 
 export const tracks: Tracks = alltracks; // from external .json file
 
-export const trackIds: TrackId[] = Object.keys(tracks)
+export const trackIds: TrackId[] = Object.keys(tracks);
 
 export const categoryIds: Set<string> = trackIds.reduce((set, trackId) => {
-  set.add(tracks[trackId].category)
-  return set
-}, new Set())
+  set.add(tracks[trackId].category);
+  return set;
+}, new Set());
 
 export const categoryPointsFromMilestoneMap = (milestoneMap: MilestoneMap) => {
-  let pointsByCategory = new Map()
+  let pointsByCategory = new Map();
   trackIds.forEach((trackId) => {
-    const milestone = milestoneMap[trackId]
-    const categoryId = tracks[trackId].category
-    let currentPoints = pointsByCategory.get(categoryId) || 0
-    pointsByCategory.set(categoryId, currentPoints + milestoneToPoints(milestone))
-  })
-  return Array.from(categoryIds.values()).map(categoryId => {
-    const points = pointsByCategory.get(categoryId)
-    return { categoryId, points: pointsByCategory.get(categoryId) || 0 }
-  })
-}
+    const milestone = milestoneMap[trackId];
+    const categoryId = tracks[trackId].category;
+    let currentPoints = pointsByCategory.get(categoryId) || 0;
+    pointsByCategory.set(
+      categoryId,
+      currentPoints + milestoneToPoints(milestone)
+    );
+  });
+  return Array.from(categoryIds.values()).map((categoryId) => {
+    const points = pointsByCategory.get(categoryId);
+    return { categoryId, points: pointsByCategory.get(categoryId) || 0 };
+  });
+};
 
-export const totalPointsFromMilestoneMap = (milestoneMap: MilestoneMap): number =>
-  trackIds.map(trackId => milestoneToPoints(milestoneMap[trackId]))
-    .reduce((sum, addend) => (sum + addend), 0)
+export const totalPointsFromMilestoneMap = (
+  milestoneMap: MilestoneMap
+): number =>
+  trackIds
+    .map((trackId) => milestoneToPoints(milestoneMap[trackId]))
+    .reduce((sum, addend) => sum + addend, 0);
 
-export const categoryColorScale = d3.scaleOrdinal()
+export const categoryColorScale = d3
+  .scaleOrdinal()
   .domain(categoryIds)
-  .range(['#00b8a9', '#f8f3d4', '#f6416c', '#ffde7d'])
+  .range(["#2ca6c9", "rgb(203 234 255)", "#2c76a8", "#a3d3e8"]);
 
 // export const titles = [
 //   {label: 'Engineer I', minPoints: 0, maxPoints: 16},
@@ -137,18 +164,22 @@ export const categoryColorScale = d3.scaleOrdinal()
 // ]
 
 export const titles = [
-  {label: 'Thrillworker', minPoints: 0, maxPoints: 16},
-  {label: 'SuperThrillworker', minPoints: 12, maxPoints: 30},
-  {label: 'SuperDuperThrillworker', minPoints: 25, maxPoints: 57},
-  {label: 'DoubleSuperDuperThrillworker', minPoints: 50, maxPoints: 75},
-  {label: 'DoublePlusSuperDuperThrillworker', minPoints: 76, maxPoints: 89},
-  {label: 'Doug', minPoints: 90},
-]
+  { label: "Thrillworker", minPoints: 0, maxPoints: 16 },
+  { label: "SuperThrillworker", minPoints: 12, maxPoints: 30 },
+  { label: "SuperDuperThrillworker", minPoints: 25, maxPoints: 57 },
+  { label: "DoubleSuperDuperThrillworker", minPoints: 50, maxPoints: 75 },
+  { label: "DoublePlusSuperDuperThrillworker", minPoints: 76, maxPoints: 89 },
+  { label: "Doug", minPoints: 90 }
+];
 
 export const eligibleTitles = (milestoneMap: MilestoneMap): string[] => {
-  const totalPoints = totalPointsFromMilestoneMap(milestoneMap)
+  const totalPoints = totalPointsFromMilestoneMap(milestoneMap);
 
-  return titles.filter(title => (title.minPoints === undefined || totalPoints >= title.minPoints)
-                             && (title.maxPoints === undefined || totalPoints <= title.maxPoints))
-    .map(title => title.label)
-}
+  return titles
+    .filter(
+      (title) =>
+        (title.minPoints === undefined || totalPoints >= title.minPoints) &&
+        (title.maxPoints === undefined || totalPoints <= title.maxPoints)
+    )
+    .map((title) => title.label);
+};
